258f4ddf3bba3a77df0470d140d6f03dfd21adef
Richard Leys <richard.leys@kit.edu>
2024-02-28 13:31:48 +0100

    FW Verification: improved top level test to add autoread sequence to check SPI clock output

--- fw/astep24-3l/target-multiboard/sim_top.py
+++ fw/astep24-3l/target-multiboard/sim_top.py
@@ -11,7 +11,7 @@ import rfg
 import astep24_3l_sim
 
 
-@cocotb.test(timeout_time = 1,timeout_unit="ms",skip=True)
+@cocotb.test(timeout_time = 1,timeout_unit="ms")
 async def test_clocking_resets(dut):
     await vip.cctb.common_clock_reset_nexys(dut)
 
@@ -31,7 +31,7 @@ async def test_read_id(dut):
     print("Version: ",version)
     await Timer(10, units="us")
 
-@cocotb.test(timeout_time = 12,timeout_unit="ms",skip=True)
+@cocotb.test(timeout_time = 12,timeout_unit="ms")
 async def test_injection(dut):
 
     rfg.core.debug()
@@ -60,9 +60,11 @@ async def test_injection(dut):
     await Timer(500, units="us")
 
 
-@cocotb.test(timeout_time = 1,timeout_unit="ms")
+
+@cocotb.test(timeout_time = 2,timeout_unit="ms")
 async def test_spi_1byte_out(dut):
 
+    dut.layer_0_interruptn.value = 1
     boardDriver = astep24_3l_sim.getUARTDriver(dut)
     await vip.cctb.common_clock_reset_nexys(dut)
     await Timer(10, units="us")
@@ -76,8 +78,18 @@ async def test_spi_1byte_out(dut):
     await boardDriver.configureLayerSPIFrequency(500000,flush=True)
     await Timer(50, units="us")
 
-    ## Write bytes
-    await boardDriver.setLayerReset(0,reset = False , flush = True)
+
+    ## Write bytes to get view on the output
+    await boardDriver.setLayerConfig(0,reset = False , autoread = False, hold = True, flush = True)
+    await boardDriver.writeBytesToLayer(0,[0xAB,0xCD],flush=True)
+    await Timer(100, units="us")
+
+    ## Write bytes to get view on the output
+    await boardDriver.setLayerConfig(0,reset = False , autoread = True, hold = True, flush = True)
     await boardDriver.writeBytesToLayer(0,[0xAB,0xCD],flush=True)
 
-    await Timer(200, units="us")
+    ## Trigger some interrupt based bytes to see if clock changes
+    dut.layer_0_interruptn.value = 0
+    await Timer(100, units="us")
+    dut.layer_0_interruptn.value = 1
+    await Timer(100, units="us")
